50ce210653be3c80dd97ca58427e467498d6f21b
SadMadLad <zeroinside4u@gmail.com>
2023-03-26 01:03:02 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import authRoutes from './routes/authRoutes.js';
+
+dotenv.config()
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+/* ROUTES */
+app.use('/auth', authRoutes);
+
+/* MONGODB AND PORT */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log(`SERVER: ${PORT}`)))
+  .catch(error => console.log(error))
