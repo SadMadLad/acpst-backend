306ad473970ecc26c9615527289ec086e865d5f8
Saad Azam <zeroinside4u@gmail.com>
2023-03-26 05:03:23 +0500

    Remove cyclic change

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-// import cors from 'cors';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
 import authRoutes from './routes/authRoutes.js';
-
-/* CONFIG FOR CYCLIC */
-const PORT = process.env.PORT || 6001;
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
 
 dotenv.config()
 
@@ -32,14 +17,17 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-// app.use(cors());
+app.use(cors());
 
 /* ROUTES */
 app.use('/auth', authRoutes);
 
 /* MONGODB AND PORT */
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("listening for requests");
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-})
+  .then(() => app.listen(PORT, () => console.log(`SERVER: ${PORT}`)))
+  .catch(error => console.log(error))
